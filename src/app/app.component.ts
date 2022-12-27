fd1e1432e4226c6e1a78aa79690d00fda65992c6
Jeremiah Malicdem <jmalicdem45@gmail.com>
2022-12-27 14:26:48 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Jeremiah Malicdem';
+
+  workExperiences = [
+    {
+      title: 'Frontend Angular Developer',
+      company: 'Offshorly',
+      date: 'January 2023 - Present',
+      stack: [
+        'Angular'
+      ]
+    },
+    {
+      title: 'Full Stack Developer',
+      company: 'Firstmac Ltd.',
+      date: 'May 2022 - January 2023',
+      stack: [
+        'Angular',
+        'Ionic',
+        'NodeJs',
+        'AWS',
+        'MongoDb',
+        "PostgreSql"
+      ]
+    },
+    {
+      company: 'Senior Software Engineer',
+      title: 'HCL Technologies Ltd.',
+      date: 'July 2021 - May 2022',
+      stack: [
+        'React',
+        'AWS',
+        'GrapesJs',
+        'Stencil'
+      ]
+    },
+    {
+      title: 'Full Stack Developer',
+      company: 'August 99, Inc.',
+      date: 'November 2020 - July 2021',
+      stack: [
+        'Angular',
+        'Express',
+        'AWS',
+        'Kubernetes',
+        'Linux'
+      ]
+    },
+    {
+      title: 'Junior Web Developer',
+      company: 'FFUF Manila Inc.',
+      date: 'January 2019 - November 2020',
+      stack: [
+        'Angular',
+        'PHP',
+        'Azure',
+        'Symfony'
+      ]
+    }
+  ]
 }
